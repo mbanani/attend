bb5cc7003c240f3d05a8ff439f37f707bf42ca1f
Mohamed El Banani <mbanani@umich.edu>
2016-11-08 15:01:54 -0500

    cpp/matlab interface, not working yet

--- src/edges/edgeBoxesDemo.m
+++ src/edges/edgeBoxesDemo.m
@@ -16,10 +16,8 @@ I = imread('peppers.png');
 tic, bbs=edgeBoxes(I,model,opts); toc
 
 %% show evaluation results (using pre-defined or interactive boxes)
-gt=[122 248 92 65; 193 82 71 53; 410 237 101 81; 204 160 114 95; ...
-  9 185 86 90; 389 93 120 117; 253 103 107 57; 81 140 91 63];
-if(0), gt='Please select an object box.'; disp(gt); figure(1); imshow(I);
-  title(gt); [~,gt]=imRectRot('rotate',0); gt=gt.getPos(); end
+gt=[];
+
 gt(:,5)=0; [gtRes,dtRes]=bbGt('evalRes',gt,double(bbs),.7);
 figure(1); bbGt('showRes',I,gtRes,dtRes(dtRes(:,6)==1,:));
 title('green=matched gt  red=missed gt  dashed-green=matched detect');
@@ -29,5 +27,3 @@ if(~exist('boxes/VOCdevkit/','dir')), return; end
 split='val'; data=boxesData('split',split);
 nm='EdgeBoxes70'; opts.name=['boxes/' nm '-' split '.mat'];
 edgeBoxes(data.imgs,model,opts); opts.name=[];
-boxesEval('data',data,'names',nm,'thrs',.7,'show',2);
-boxesEval('data',data,'names',nm,'thrs',.5:.05:1,'cnts',1000,'show',3);
